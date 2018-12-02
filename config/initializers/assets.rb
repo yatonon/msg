4161a3205d2d789fc8b0396a48a70360ace15585
河原拓眞 <k.takuma@kawaharatakushinnoMacBook-ea.local>
2018-12-03 00:10:53 +0900

    プロフィール編集画面の見た目を変更

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,6 +4,7 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w(pages.css)
 Rails.application.config.assets.precompile += %w(users.css)
+Rails.application.config.assets.precompile += %w(edit.css)
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
