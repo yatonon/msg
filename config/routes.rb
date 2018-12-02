a0356f909783b0c9b64b0164218ef521deade891
K.Kiyokawa <koichi20110068@gmail.com>
2018-12-02 18:42:03 +0900

    チャンネル画面　招待機能

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'page/home'
   resources :users
-  resources :channels
+  resources :channels do
+    member do
+      get 'invite_user_index'
+      post 'invite_user'
+    end
+  end
   resources :messages, only: %i(create edit update destroy)
 end
