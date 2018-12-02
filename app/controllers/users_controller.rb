e2e4109ef827974e7a07ebc1ce9f2000ccae6c80
yatonon <yatonon@yahoo.co.jp>
2018-12-03 00:54:43 +0900

    一旦コミット

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,14 @@ class UsersController < ApplicationController
 
   def profile
   end
+
+  def update
+    current_user.update(name: "s")
+    current_user.update(email: current_user.email)
+    if current_user.user_original?
+      current_user.update(user_original: current_user.user_original)
+    else
+    redirect_to profile_path
+    end
+  end
 end
