d12bbc04523c0268721ec508d932437239c1aa0b
K.Kiyokawa <koichi20110068@gmail.com>
2018-12-03 15:47:23 +0900

    エラーメッセージ対応

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   def index
-        @users = User.all
+    @users = User.all
   end
 
   def edit
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
     if current_user.update(params.require(:user).permit(:name, :email, :user_original))
       redirect_to profile_path
     else
-      redirect_to edit_user_path
+      render :edit
     end
   end
 end
