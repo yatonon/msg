113daec821cd0f8c2e6208eaa7588e63b410c2d5
yatonon <yatonon@yahoo.co.jp>
2018-12-04 23:50:22 +0900

    バリデーション追加

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,5 +1,11 @@
 class Friend < ApplicationRecord
   belongs_to :user
 
-  validates :user_id,:uniqueness => {:scope =>[:to_id, :] }
+  # validates :user_id,uniqueness:true
+  validates :user_id,:uniqueness => {:scope =>[:to_id]}
+
+  with_options if: :establish? do
+    validates :establish,  inclusion: { in: [false] }
+    validates :apply,  inclusion: { in: [true] }
+  end
 end
