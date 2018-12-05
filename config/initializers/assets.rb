5e71eff338551a865c2aacea113ea57e6b083deb
河原拓眞 <k.takuma@kawaharatakushinnoMacBook-ea.local>
2018-12-06 00:37:05 +0900

    トーク画面の見た目を変更

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,6 +5,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w(pages.css)
 Rails.application.config.assets.precompile += %w(users.css)
 Rails.application.config.assets.precompile += %w(edit.css)
+Rails.application.config.assets.precompile += %w(channel.css)
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
