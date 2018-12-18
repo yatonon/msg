2333797789609edc3468f356b9805e9f8a532e0f
yatonon <yatonon@yahoo.co.jp>
2018-12-18 11:40:53 +0900

    thumbを保存

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,5 @@
+require 'mini_magick'
+
 class UsersController < ApplicationController
   before_action :authenticate_user!
   def index
@@ -12,9 +14,27 @@ class UsersController < ApplicationController
 
   def update
     if current_user.update(params.require(:user).permit(:name, :email, :user_original, :icon_img))
+      if params[:user][:icon_img] != nil
+        img_resize(300, 300)
+        thumb_resize("public/uploads/user/#{params[:user][:icon_img].original_filename}", 100, 100)
+      end
       redirect_to profile_path
     else
       render :edit
     end
   end
+  private
+  def img_resize(width, height)
+    img = MiniMagick::Image.read(params[:user][:icon_img])
+    img.gravity(:center)
+    img.resize "#{width}x#{height}"
+    img.write "public/uploads/user/#{params[:user][:icon_img].original_filename}"
+  end
+  def thumb_resize(path, width, height)
+    img = MiniMagick::Image.open(path)
+    img.gravity(:center)
+    img.resize "#{width}x#{height}"
+    img.write "public/uploads/user/thumb_#{params[:user][:icon_img].original_filename}"
+  end
+  
 end
