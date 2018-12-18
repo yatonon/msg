1fdee850aab41f9a8de8a56b80651ec619914152
yatonon <yatonon@yahoo.co.jp>
2018-12-18 13:57:18 +0900

    アイコンを追加（日本語ファイル非対応）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
   def update
     if current_user.update(params.require(:user).permit(:name, :email, :user_original, :icon_img))
       if params[:user][:icon_img] != nil
-        img_resize(300, 300)
+        img_resize(400, 400)
         thumb_resize("public/uploads/user/#{params[:user][:icon_img].original_filename}", 100, 100)
       end
       redirect_to profile_path
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
     img = MiniMagick::Image.open(path)
     img.gravity(:center)
     img.resize "#{width}x#{height}"
-    img.write "public/uploads/user/thumb_#{params[:user][:icon_img].original_filename}"
+    img.write "public/uploads/user/thumb/#{params[:user][:icon_img].original_filename}"
   end
-  
+
 end
