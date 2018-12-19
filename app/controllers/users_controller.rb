fe05b33e96a3cf5f3fd6da63570feb4537b2f24e
K.Kiyokawa <koichi20110068@gmail.com>
2018-12-19 21:52:51 +0900

    strong parameterの書き方変えただけ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   end
 
   def update
-    if current_user.update(params.require(:user).permit(:name, :email, :user_original, :icon_img))
+    if current_user.update(user_params)
       if params[:user][:icon_img] != nil
         img_resize(400, 400)
         thumb_resize("public/uploads/user/#{params[:user][:icon_img].original_filename}", 100, 100)
@@ -23,18 +23,24 @@ class UsersController < ApplicationController
       render :edit
     end
   end
-  private
-  def img_resize(width, height)
-    img = MiniMagick::Image.read(params[:user][:icon_img])
-    img.gravity(:center)
-    img.resize "#{width}x#{height}"
-    img.write "public/uploads/user/#{params[:user][:icon_img].original_filename}"
-  end
-  def thumb_resize(path, width, height)
-    img = MiniMagick::Image.open(path)
-    img.gravity(:center)
-    img.resize "#{width}x#{height}"
-    img.write "public/uploads/user/thumb/#{params[:user][:icon_img].original_filename}"
-  end
 
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :user_original, :icon_img)
+    end
+
+    def img_resize(width, height)
+      img = MiniMagick::Image.read(params[:user][:icon_img])
+      img.gravity(:center)
+      img.resize "#{width}x#{height}"
+      img.write "public/uploads/user/#{params[:user][:icon_img].original_filename}"
+    end
+
+    def thumb_resize(path, width, height)
+      img = MiniMagick::Image.open(path)
+      img.gravity(:center)
+      img.resize "#{width}x#{height}"
+      img.write "public/uploads/user/thumb/#{params[:user][:icon_img].original_filename}"
+    end
 end
