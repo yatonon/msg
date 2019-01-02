b5c72580a0ad74768675c6d72fd826988314cda7
yatonon <yatonon@yahoo.co.jp>
2019-01-02 19:49:34 +0900

    user_index をフレンド一覧に変更＆フレンド検索を一覧に追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,11 @@ require 'mini_magick'
 class UsersController < ApplicationController
   before_action :authenticate_user!
   def index
-    @users = User.all
+    your_friend_ids = current_user.friends.established.pluck(:to_id) +  # 自分から申請して友だちになったやつ
+      User.joins(:friends)  # 申請を受け入れて友達になったやつ
+        .where(friends: { to_id: current_user.id, establish: true })
+        .pluck(:user_id)
+    @your_friends = User.where(id: your_friend_ids).order(:name)
   end
 
   def edit
